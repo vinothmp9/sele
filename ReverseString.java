c78f98f660a526f84852dc3af8cf3691688287f6
vinothmp9 <vinothpandianecb@gmail.com>
2020-03-09 11:43:38 +0530

    initial commit

---
+++ ReverseString.java
@@ -0,0 +1,22 @@
+package important;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String word = "Happy";
+		String reverse = "";
+	
+		//method 1
+		int length = word.length();
+		for (int i = length - 1; i >= 0; i--) {
+			reverse = reverse + word.charAt(i);
+		}
+		System.out.print(reverse);
+ 
+		
+		//method 2:
+		StringBuffer rev=new StringBuffer(word);
+		System.out.println(rev.reverse());
+	}
+}
