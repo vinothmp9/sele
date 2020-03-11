645de560e805aa5387acf8d7f4149abd13c074bc
vinothmp9 <vinothpandian0109@gmail.com>
2020-03-11 09:19:45 +0530

    initial commit

---
+++ HelloProject.java
@@ -0,0 +1,14 @@
+package com.student.org;
+
+public class HelloProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello project");
+System.out.println("hello project");
+System.out.println("hello project");
+System.out.println("hello project");
+
+	}
+
+}
